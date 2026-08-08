0bb3020f2ff9d588a2afc424f7428d07bf415ccd
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2026-08-08 17:02:14 +1200

    remove multi-threading

--- table/covering_texture.gd
+++ table/covering_texture.gd
@@ -3,28 +3,12 @@ extends Sprite2D
 
 
 var luminance: float = 1.0
-#var _lum_lock := Mutex.new()
-#
-#var _update_thread := Thread.new()
-#var _update_semaphore := Semaphore.new()
-#var _deletion_mutex := Mutex.new()
-#var _should_delete := false
-#var _texture_mutex := Mutex.new()
 
 @onready var progress_bar: ProgressBar = $ProgressBar
 
 
 func _ready() -> void:
-	#_update_thread.start(_update_cover_threaded)
 	setup_new_layer()
-
-
-#func _exit_tree() -> void:
-	#_deletion_mutex.lock()
-	#_should_delete = true
-	#_deletion_mutex.unlock()
-	#_update_semaphore.post()
-	#_update_thread.wait_to_finish()
 
 
 func setup_new_layer():
@@ -52,38 +36,3 @@ func _on_check_cover_timeout() -> void:
 	print(lum)
 	luminance = lum
 	progress_bar.value = luminance
-	#return
-	#_update_semaphore.post()
-	#_lum_lock.lock()
-	#progress_bar.value = luminance
-	#_lum_lock.unlock()
-#
-#
-#func _update_cover_threaded() -> void:
-	#while true:
-		## Wait until it should update
-		#_update_semaphore.wait()
-		#
-		## Check if it should be deleted
-		#_deletion_mutex.lock()
-		#var should_delete: bool = _should_delete
-		#_deletion_mutex.unlock()
-		#if should_delete:
-			#return
-		#
-		## Get texture image
-		#_texture_mutex.lock()
-		#var image: Image = texture.get_image()
-		#_texture_mutex.unlock()
-		#
-		#var pixels: PackedColorArray = image.get_data().to_color_array()
-		#var lum: float = 0.0
-		#
-		#for color: Color in pixels:
-			#lum += color.get_luminance()
-		#
-		#lum /= image.get_size().x * image.get_size().y
-		#print(lum)
-		#_lum_lock.lock()
-		#luminance = lum
-		#_lum_lock.unlock()
